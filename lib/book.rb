702277ea034880ef350c27c7e2283e5c73b13155
jyliu17 <jyliu17@gmail.com>
2020-10-27 19:17:00 -0400

    done

--- lib/book.rb
+++ lib/book.rb
@@ -4,13 +4,12 @@ class Book
     attr_accessor :author, :page_count, :genre
 
     def initialize(title)
-       @title = title
-    end
-    
-    def turn_page
-        puts "Flipping the page...wow, you read fast!"
+        @title = title
     end
 
+    def turn_page
+     puts "Flipping the page...wow, you read fast!"
+    end
 
 
 end
